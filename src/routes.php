cd3d68cb6cfc3066d5038ec82b8bb13b90f42121
Ron Melkhior <ronm@hey.com>
2023-08-01 01:19:32 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -10,6 +10,3 @@ Route::domain(config('laravel-passwordless-login.routes_domain', null))
         [LaravelPasswordlessLoginController::class, 'login']
     )
     ->middleware(config('laravel-passwordless-login.middleware', ['web', HandleAuthenticatedUsers::class]))->name(config('laravel-passwordless-login.login_route_name'));
-
-Route::domain(config('routes_domain', null))->get('/laravel_passwordless_login_redirect_test_route', [LaravelPasswordlessLoginController::class, 'redirectTestRoute'])->middleware('auth');
-Route::domain(config('routes_domain', null))->get('/laravel_passwordless_login_redirect_overridden_route', [LaravelPasswordlessLoginController::class, 'redirectTestRoute'])->middleware('auth');
